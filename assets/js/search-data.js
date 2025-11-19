2ee3075de75df1290bd988cf6345a0f3cc1798ef
QueenaWang <83569857+QueenaWang@users.noreply.github.com>
2025-11-19 20:44:06 +0000

    Deploying to gh-pages from @ QueenaWang/QueenaWang.github.io@6129fc4c38119290d3c975cc504e58cc993b0495 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Professional experience and academic background in actuarial science, finance, and data analytics.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
